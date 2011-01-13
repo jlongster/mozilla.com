e64ccbce1a3be36d7d5e9164d3dc27bf35b834f7
pascal.chevrel@mozilla-europe.org <pascal.chevrel@mozilla-europe.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-01-13 22:04:44 +0000

    l10n: Fx4 beta 9 whatsnew page and associated files (subtitles and reusable sidebar) for all locales that had a beta 8
    git-svn-id: https://svn.mozilla.org/projects/mozilla.com/trunk@80771 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ ta-LK/includes/l10n/videos/sub-fx4-panorama.php
@@ -0,0 +1,28 @@
+<?php
+
+$sub[1]  = "<em style='display:block; margin-top:-20px;'>How to organize your tabs</em>Firefox Panorama gives you a visual overview of all of your tabs&hellip;";
+$sub[2]  = "&hellip;and lets organize them into groups so that you can organize your workflow and keep separate tasks separate.";
+$sub[3]  = "As you've already discovered you can use the Panorama button to begin organizing your tabs.";
+$sub[4]  = "You can then click on a tab, or hit Escape to go back.";
+$sub[5]  = "Of course you can click any tab to activate that tab.";
+$sub[6]  = "Alternatively, you can use option-space on Mac or control-space on Windows to enter and exit Firefox Panorama.";
+$sub[7]  = "To group tabs simply drag two together.";
+$sub[8]  = "In this case, I want to group all of my pictures of space.";
+$sub[9]  = "";
+$sub[10] = "I can also click and drag an empty space to make a group.";
+$sub[11] = "When you zoom into a group, you only see the tabs that are in that group. This helps you focus.";
+$sub[12] = "To organize your groups, simply drag them around.";
+$sub[13] = "You can change their size so that less important things are smaller&hellip;";
+$sub[14] = "&hellip;and more important things are bigger.";
+$sub[15] = "If you make a group small enough, it will turn into a pile.";
+$sub[16] = "You can click on a tab to open it";
+$sub[17] = "or";
+$sub[18] = "if you want a quick-look into the pile&hellip;";
+$sub[19] = "&hellip;click the expand icon&hellip;";
+$sub[20] = "&hellip;and then click the tab that you are interested in.";
+$sub[21] = "This has been a brief introduction to Firefox Panorama.";
+$sub[22] = "While there's still more to discover (like how to name groups)&hellip;";
+$sub[23] = "&hellip;you are now ready to begin exploring a better way to browse.";
+$sub[24] = "";
+
+?>
