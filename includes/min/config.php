366fb7dd5d30891d8d432a2e002f2e5214647817
pascal.chevrel@mozilla-europe.org <pascal.chevrel@mozilla-europe.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-01-13 23:00:57 +0000

    bad commit, revert config files to their previous state
    git-svn-id: https://svn.mozilla.org/projects/mozilla.com/trunk@80780 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/min/config.php
+++ includes/min/config.php
@@ -40,7 +40,7 @@ $min_enableBuilder = true;
  * will have to load extra code to guess. Some examples below:
  */
 //$min_cachePath = 'c:\\WINDOWS\\Temp';
-$min_cachePath = '/home/pascalc/dev/mozillacom/tmp';
+//$min_cachePath = '/tmp';
 //$min_cachePath = preg_replace('/^\\d+;/', '', session_save_path());
 
 
