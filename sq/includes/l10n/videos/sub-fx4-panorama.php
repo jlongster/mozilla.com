59700cc44bbbd7dcee02f9481968ceb59968160f
besnik@programeshqip.org <besnik@programeshqip.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-01-14 09:22:02 +0000

    first draft
    git-svn-id: https://svn.mozilla.org/projects/mozilla.com/trunk@80820 4eb1ac78-321c-0410-a911-ec516a8615a5

--- sq/includes/l10n/videos/sub-fx4-panorama.php
+++ sq/includes/l10n/videos/sub-fx4-panorama.php
@@ -1,28 +1,28 @@
 <?php
 
-$sub[1]  = "<em style='display:block; margin-top:-20px;'>How to organize your tabs</em>Firefox Panorama gives you a visual overview of all of your tabs&hellip;";
-$sub[2]  = "&hellip;and lets organize them into groups so that you can organize your workflow and keep separate tasks separate.";
-$sub[3]  = "As you've already discovered you can use the Panorama button to begin organizing your tabs.";
-$sub[4]  = "You can then click on a tab, or hit Escape to go back.";
-$sub[5]  = "Of course you can click any tab to activate that tab.";
-$sub[6]  = "Alternatively, you can use option-space on Mac or control-space on Windows to enter and exit Firefox Panorama.";
-$sub[7]  = "To group tabs simply drag two together.";
-$sub[8]  = "In this case, I want to group all of my pictures of space.";
+$sub[1]  = "<em style='display:block; margin-top:-20px;'>Si t'i organizoni skedat tuaja</em>Firefox Panorama ju jep një përmbledhje pamore të krejt skedave tuaja&hellip;";
+$sub[2]  = "&hellip;dhe ju lejon t'i organizoni ato në grupe që kështu të mund të organizoni rrjedhën e punës dhe t'i mbani ndaras punët e ndara.";
+$sub[3]  = "Siç edhe e keni parë, butonin Panorama mund ta përdorni për të filluar me organizimin e skedave tuaja.";
+$sub[4]  = "Mandej mund të klikoni mbi një skedë, ose të shtypni tastin që të ktheheni mbrapsht.";
+$sub[5]  = "Sigurisht që mund të klikoni mbi cilëndo skedë dhe do të aktivizohet ajo skedë.";
+$sub[6]  = "Në të njëjtën mënyrë, mund të përdorni shkurtoret Option-Space në Mac ose Control-Space në Windows që të hyni ose dilni nga mënyra Firefox Panorama.";
+$sub[7]  = "Për grupim skedash thjesht tërhiqni dy njëherazi.";
+$sub[8]  = "Në këtë rast, dua të grupoj krejt fotot e mia në hapësirë.";
 $sub[9]  = "";
-$sub[10] = "I can also click and drag an empty space to make a group.";
-$sub[11] = "When you zoom into a group, you only see the tabs that are in that group. This helps you focus.";
-$sub[12] = "To organize your groups, simply drag them around.";
-$sub[13] = "You can change their size so that less important things are smaller&hellip;";
-$sub[14] = "&hellip;and more important things are bigger.";
-$sub[15] = "If you make a group small enough, it will turn into a pile.";
-$sub[16] = "You can click on a tab to open it";
-$sub[17] = "or";
-$sub[18] = "if you want a quick-look into the pile&hellip;";
-$sub[19] = "&hellip;click the expand icon&hellip;";
-$sub[20] = "&hellip;and then click the tab that you are interested in.";
-$sub[21] = "This has been a brief introduction to Firefox Panorama.";
-$sub[22] = "While there's still more to discover (like how to name groups)&hellip;";
-$sub[23] = "&hellip;you are now ready to begin exploring a better way to browse.";
+$sub[10] = "Mundem edhe të klikoj dhe tërheq një hapësirë të zbrazët, që të krijoj një grup.";
+$sub[11] = "Kur kryeni zoom mbi një grup, shihni vetëm skedat që janë pjesë e atij grupi. Kjo ju ndihmon të përqendroheni.";
+$sub[12] = "Për sistemimin e grupeve tuaja, thjesht lëvizini atje ku doni.";
+$sub[13] = "Madhësinë e tyre mund ta ndryshoni, që gjërat me më pak rëndësi të zënë më pak vend&hellip;";
+$sub[14] = "&hellip;dhe gjërat më të rëndësishme të zënë më shumë.";
+$sub[15] = "Nëse e zvogëloni mjaft një grup, do të shndërrohet në një dëng.";
+$sub[16] = "Për ta hapur mund të klikoni mbi një skedë";
+$sub[17] = "ose,";
+$sub[18] = "nëse doni t'i hidhni një sy dëngut&hellip;";
+$sub[19] = "&hellip;klikoni mbi ikonën e zgjerimit&hellip;";
+$sub[20] = "&hellip;dhe mandej klikoni skedën që ju intereson.";
+$sub[21] = "Kjo ishte një hyrje e shkurtër te Firefox Panorama.";
+$sub[22] = "Ka edhe gjëra të tjera për t'u zbuluar (p.sh. si të emërtohen grupet)&hellip;";
+$sub[23] = "&hellip;dhe tani jeni gati të filloni eksplorimin e një mënyre më të mirë shfletimi.";
 $sub[24] = "";
 
 ?>
