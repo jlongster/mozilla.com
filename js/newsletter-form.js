1d5a04b4b4767e4948bb83a7b7c14edc45e46d5c
mike@silverorange.com <mike@silverorange.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-01-18 19:57:50 +0000

    Updated newsletter form interaction for firstrun and whatsnew pages for FF 3.6. Refs Bug #620554 and Bug #623997.
    git-svn-id: https://svn.mozilla.org/projects/mozilla.com/trunk@81143 4eb1ac78-321c-0410-a911-ec516a8615a5

--- js/newsletter-form.js
+++ js/newsletter-form.js
@@ -1,12 +1,47 @@
 $(document).ready(function() {
-  $('#open-pane').click(function(e) {
-    e.preventDefault();
-    $(this).hide();
-    $('#form-pane').fadeIn();
-    $('#form-pane input[name=email]').focus();
 
-    var uri = $(this).attr('data-wt_uri');
-    var ti = $(this).attr('data-wt_ti');
-    dcsMultiTrack('DCS.dcsuri', uri, 'WT.ti', ti);
-  });
+	var opened = (function() {
+		var ul = $('#email-form ul');
+		return ul.hasClass('error');
+	})();
+
+	function scrollTo(id, time)
+	{
+		$('html, body')
+			.animate(
+				{ scrollTop: $(id).offset().top }, time
+			);
+	}
+
+	function open()
+	{
+		if (!opened) {
+			scrollTo('#email-form', 500);
+			$('#whatsnew #newsletter').css('height', 'auto');
+			$('#form-pane').fadeIn();
+			$('#form-pane select[name=country]').focus();
+
+			opened = true;
+		}
+	}
+
+	$('#email-form').submit(function(e) {
+		if (!opened) {
+			e.preventDefault();
+			open();
+		}
+	});
+
+	$('#email-open').click(function(e) {
+		e.preventDefault();
+
+		if (!opened) {
+			$(this).addClass('opened');
+			var uri = $(this).attr('data-wt_uri');
+			var ti = $(this).attr('data-wt_ti');
+			dcsMultiTrack('DCS.dcsuri', uri, 'WT.ti', ti);
+		}
+
+		open();
+	});
 });
