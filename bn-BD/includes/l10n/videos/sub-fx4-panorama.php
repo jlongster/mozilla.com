2f2fa909a7a7d8d7dd1e3e5b104482afc09ddf4c
pascal.chevrel@mozilla-europe.org <pascal.chevrel@mozilla-europe.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-01-25 12:13:46 +0000

    l10n: beta 9 translation by Israt Jahan
    git-svn-id: https://svn.mozilla.org/projects/mozilla.com/trunk@81514 4eb1ac78-321c-0410-a911-ec516a8615a5

--- bn-BD/includes/l10n/videos/sub-fx4-panorama.php
+++ bn-BD/includes/l10n/videos/sub-fx4-panorama.php
@@ -1,28 +1,28 @@
 <?php
 
-$sub[1]  = "<em style='display:block; margin-top:-20px;'>How to organize your tabs</em>Firefox Panorama gives you a visual overview of all of your tabs&hellip;";
-$sub[2]  = "&hellip;and lets organize them into groups so that you can organize your workflow and keep separate tasks separate.";
-$sub[3]  = "As you've already discovered you can use the Panorama button to begin organizing your tabs.";
-$sub[4]  = "You can then click on a tab, or hit Escape to go back.";
-$sub[5]  = "Of course you can click any tab to activate that tab.";
-$sub[6]  = "Alternatively, you can use option-space on Mac or control-space on Windows to enter and exit Firefox Panorama.";
-$sub[7]  = "To group tabs simply drag two together.";
-$sub[8]  = "In this case, I want to group all of my pictures of space.";
+$sub[1]  = "<em style='display:block; margin-top:-20px;'>আপনার ট্যাব কিভাবে সাজানো হবে</em>ফায়ারফক্স প্যানোরামা আপনার সব tabs&hellip; এর একটি দৃশ্যমান সারসংক্ষেপ প্রকাশ করে";
+$sub[2]  = "&hellip;এবং গ্রুপ আকারে সাজিয়ে নেয়া হয় যাতে করে আপনি আপনার কাজের ধারাও সাজিয়ে নিতে পারেন এবং বিভিন্ন কাজ আলাদা করে রাখতে পারেন।";
+$sub[3]  = "আপনি ইতোমধ্যে আবিস্কার করে ফেলেছেন যে আপনার ট্যাবগুলো সাজানোর জন্য প্যানোরামা বোতামে চাপা যায়।";
+$sub[4]  = "আপনি কোনো একটি ট্যাবে ক্লিক করে, অথবা Escape চেপে পূর্বাবস্থায় ফিরে যেতে পারেন।";
+$sub[5]  = "আপনি অবশ্যিই যে কোনো ট্যাবে ক্লিক করে তা সক্রিয় করে নিতে পারেন।";
+$sub[6]  = "অপরপক্ষে, Mac এর ক্ষেত্রে space বা উইন্ডোজ এর ক্ষেত্রে control-space অপশন ব্যবহার করে ফায়ারফক্স প্যানোরামা ব্যবহার করা বা বন্ধ করা যায়।";
+$sub[7]  = "দুটি ট্যাবকে একই গ্রুপে আনার জন টেনে এনে এক জায়গায় রাখলেই হয়।";
+$sub[8]  = "এ ক্ষেত্রে, আমি আমার সব স্পেসের ছবি গ্রুপ আকারে রাখতে চাই।";
 $sub[9]  = "";
-$sub[10] = "I can also click and drag an empty space to make a group.";
-$sub[11] = "When you zoom into a group, you only see the tabs that are in that group. This helps you focus.";
-$sub[12] = "To organize your groups, simply drag them around.";
-$sub[13] = "You can change their size so that less important things are smaller&hellip;";
-$sub[14] = "&hellip;and more important things are bigger.";
-$sub[15] = "If you make a group small enough, it will turn into a pile.";
-$sub[16] = "You can click on a tab to open it";
-$sub[17] = "or";
-$sub[18] = "if you want a quick-look into the pile&hellip;";
-$sub[19] = "&hellip;click the expand icon&hellip;";
-$sub[20] = "&hellip;and then click the tab that you are interested in.";
-$sub[21] = "This has been a brief introduction to Firefox Panorama.";
-$sub[22] = "While there's still more to discover (like how to name groups)&hellip;";
-$sub[23] = "&hellip;you are now ready to begin exploring a better way to browse.";
+$sub[10] = "ফাঁকাস্থান টেনে নিয়ে এসেও একটি গ্রুপ তৈরি করা যায়।";
+$sub[11] = "আপনি যখন কোনো গ্রুপকে বড় আকারে দেখবেন তখন ঐ গ্রুপের সবগুলো ট্যাব দেখতে পাবেন। এটা আপনাকে ফোকাস করতে সাহায্য করবে।";
+$sub[12] = "গ্রুপগুলো সাজানোর জন্য টেনে এনে পাশাপাশি রাখা হয়।";
+$sub[13] = "কম গুরুত্বপূর্ন জিনিসগুলো যাতে ছোট আকারে প্রদর্শিত হয় সেভাবে তাদের আকার পরিবর্তন করা যায়&hellip;";
+$sub[14] = "&hellip;এবং বেশি গুরুত্বপূর্ন জিনিসগুলো বড় আকারের হয়";
+$sub[15] = "যদি যথেষ্ট ছোট আকারের কোনো গ্রুপ তৈরি করেন তবে তা পাইলে রূপান্তরিত হবে।";
+$sub[16] = "কোনো ট্যাব খোলার জন্য এর উপর ক্লিক করুন";
+$sub[17] = "অথবা";
+$sub[18] = "যদি শুধু পাইলে এ চোখ বুলিয়ে যেতে চান তবে&hellip;";
+$sub[19] = "&hellip;সম্প্রসারন আইকনে ক্লিক করুন&hellip;";
+$sub[20] = "&hellip;এবং এরপর আপনি যে ট্যাবের ব্যাপারে উৎসুক তাতে ক্লিক করুন।";
+$sub[21] = "এটাই ফায়ারফক্স প্যানোরামার সংক্ষিপ্ত বর্ণনা।";
+$sub[22] = "এখনও জানার অনেক কিছু বাকি আছে (যেমন: কিভাবে গ্রুপের নামকরণ করতে হয়)&hellip;";
+$sub[23] = "&hellip;আপনি এখন আরও ভালো উপায়ে ব্রাউজ করার জন্য সম্পূর্ণরূপে তৈরি।";
 $sub[24] = "";
 
 ?>
