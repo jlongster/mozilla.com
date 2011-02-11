2572c104aefe378920e27909de21641ba0bcdbbf
pascal.chevrel@mozilla-europe.org <pascal.chevrel@mozilla-europe.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-02-11 15:18:53 +0000

    l10n: CSS improvements for about pages + remove obsolete about page include
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@82464 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/l10n/about.inc.php
+++ includes/l10n/about.inc.php
@@ -8,7 +8,20 @@ if(!isset($extra_headers)) {$extra_headers = '';}
 
 $extra_headers .= <<<EXTRA_HEADERS
     <link rel="stylesheet" type="text/css" href="{$config['static_prefix']}/style/covehead/about.css" media="screen" />
+    <style>
+    #wrapper {
+        background: url("/img/covehead/firefox/survey/thanks-background.png") no-repeat scroll 800px 150px transparent;
+    }
 
+    #content {
+        margin-right: 400px;
+        font-size: 16px;
+    }
+
+    #main-feature h2 {
+        width: 65%;
+    }
+    </style>
 EXTRA_HEADERS;
 
 include_once "{$config['file_root']}/includes/l10n/header-pages.inc.php";
