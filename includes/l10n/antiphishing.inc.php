fb76ef6abf9125a8a2793177d2788a22d8da3237
pascal.chevrel@mozilla-europe.org <pascal.chevrel@mozilla-europe.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-02-16 14:21:42 +0000

    l10n: it, put phishing protection page in the common l10n template
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@82709 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ includes/l10n/antiphishing.inc.php
@@ -0,0 +1,24 @@
+<?php
+    $body_id    = 'phishing-protection';
+    $html5      = true;
+
+    $extra_headers = <<<EXTRA_HEADERS
+    <link rel="stylesheet" type="text/css" href="{$config['static_prefix']}/style/covehead/phishing-page.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="{$config['static_prefix']}/style/covehead/mozilla-expanders.css" media="screen" />
+    <script type="text/javascript">//<![CDATA[
+        function userAgentRedirect(match_string, url) {
+            if (navigator.userAgent && navigator.userAgent.indexOf(match_string) != -1)
+                window.location = url;
+        }
+        userAgentRedirect('Firefox/2', '/{$lang}/firefox/phishing-protection/firefox2/');
+    //]]></script>
+EXTRA_HEADERS;
+
+    $extra_footers = <<<EXTRA_FOOTERS
+<script type="text/javascript" src="/js/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="/js/mozilla-expanders.js"></script>
+
+EXTRA_FOOTERS;
+
+    require "{$config['file_root']}/includes/l10n/header-pages.inc.php";
+?>
--- includes/l10n/controller.inc.php
+++ includes/l10n/controller.inc.php
@@ -1,16 +1,17 @@
 <?php
 
 // make sure we have a few variables defined to avoid php warnings if they don't exist
-$head_add         = (isset($head_add)) ? $head_add                 : $head_add = '';
-$body_class       = (isset($body_class)) ? $body_class             : $body_class = '';
-$meta_description = (isset($meta_description)) ? $meta_description : $meta_description = '';
-$extra_headers    = (isset($extra_headers)) ? $extra_headers       : $extra_headers = '';
+$head_add         = (isset($head_add)) ? $head_add                 : '';
+$body_id          = (isset($body_id)) ? $body_id                   : '';
+$body_class       = (isset($body_class)) ? $body_class             : '';
+$meta_description = (isset($meta_description)) ? $meta_description : '';
+$extra_headers    = (isset($extra_headers)) ? $extra_headers       : '';
 $page_title       = empty($page_title)    ? 'Mozilla.com'          : $page_title;
 $extra_feature    = empty($extra_feature) ? ''                     : $extra_feature;
 $extra_css        = empty($extra_css)     ? ''                     : $extra_css;
 $body_class       = empty($body_class)    ? ''                     : $body_class;
 $textdir          = (in_array($lang, array('ar', 'fa', 'he'))) ? 'rtl' : 'ltr';
-
+$footerfile       = $config['file_root'].'/includes/l10n/footer-pages.inc.php';
 
 
 // dummy function to avoid error in code borrowed from mozilla-europe
@@ -54,6 +55,7 @@ $sitepages = array(
     'unsupported'        => 'unsupported-version.inc.php',
     'firstrun-36'        => '3.6/firstrun.inc.php',
     'whatsnew-36'        => '3.6/whatsnew.inc.php',
+    'antiphishing'       => 'antiphishing.inc.php',
 );
 
 // add the include if it exists only
