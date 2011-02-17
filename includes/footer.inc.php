c186799746dcb6f29b73a9a00b94a9fa308402af
James Long <jlongster@jlongster.com>
2011-02-17 17:31:37 -0500

    begin porting newsletter signup block

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -12,7 +12,7 @@ $latest_firefox_version = LATEST_FIREFOX_VERSION;
 // Webtrends stats, see bug 556384
 require "{$config['file_root']}/includes/js_stats.inc.php";
 
-$dynamic_footer = <<<DYNAMIC_FOOTER
+?>
 
 </div><!-- end #doc -->
 </div><!-- end #wrapper -->
@@ -28,9 +28,9 @@ $dynamic_footer = <<<DYNAMIC_FOOTER
                           </a>
                         </li>
 		</ul>
-		<p id="sub-footer-newsletter">
-    { include("$config['file_root']/includes/newsletter.inc.php"); }
-		</p>
+		<div id="sub-footer-newsletter">
+    <?php require "{$config['file_root']}/includes/newsletter.inc.php"; ?>
+		</div>
 		</div>
 	</div>
 	<div id="footer">
@@ -39,34 +39,31 @@ $dynamic_footer = <<<DYNAMIC_FOOTER
 		<div id="footer-right">
 
 		<form id="lang_form" dir="ltr" method="get"><div>
-			<label for="flang">{$l10n->get('Other Languages')}</label>
-			{$lang_list}
+			<label for="flang"><?=$l10n->get('Other Languages')?></label>
+			<?=$lang_list?>
 			<noscript>
-				<div><input type="submit" id="lang_submit" value="{$l10n->get('Go')}" /></div>
+				<div><input type="submit" id="lang_submit" value="<?=$l10n->get('Go')?>" /></div>
 			</noscript>
 		</div></form>
 
 		</div>
 
-		<h3 id="footer-logo"><a href="/{$lang}/firefox/" title="{$l10n->get('Back to home page')}">{$l10n->get('Firefox')}</a></h3>
+		<h3 id="footer-logo"><a href="/<?=$lang?>/firefox/" title="<?=$l10n->get('Back to home page')?>"><?=$l10n->get('Firefox')?></a></h3>
 
-		{$dynamic_bottom_menu}
+                <?=$dynamic_bottom_menu?>
 
 		<div id="copyright">
-			<p id="footer-links"><a href="/{$lang}/privacy-policy.html">{$l10n->get('Privacy Policy')}</a> &nbsp;|&nbsp; 
-			<a href="/{$lang}/about/legal.html">{$l10n->get('Legal Notices')}</a> &nbsp;|&nbsp;
-                        <a href="/{$lang}/legal/fraud-report/index.html">{$l10n->get('Report Trademark Abuse')}</a></p>
-			<p>Except where otherwise <a href="/{$lang}/about/legal.html#site">noted</a>, content on this site is licensed under the <br /><a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution Share-Alike License v3.0</a> or any later version.</p>
+			<p id="footer-links"><a href="/<?=$lang?>/privacy-policy.html"><?=$l10n->get('Privacy Policy')?></a> &nbsp;|&nbsp; 
+			<a href="/<?=$lang?>/about/legal.html"><?=$l10n->get('Legal Notices')?></a> &nbsp;|&nbsp;
+                        <a href="/<?=$lang?>/legal/fraud-report/index.html"><?=$l10n->get('Report Trademark Abuse')?></a></p>
+			<p>Except where otherwise <a href="/<?=$lang?>/about/legal.html#site">noted</a>, content on this site is licensed under the <br /><a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution Share-Alike License v3.0</a> or any later version.</p>
 		</div>
 
 	</div>
 	</div>
 	<!-- end #footer -->
-    {$stats_js}
-	{$extra_footers}
+    <?=$stats_js?>
+	<?=$extra_footers?>
 
 </body>
 </html>
-DYNAMIC_FOOTER;
-
-?>
