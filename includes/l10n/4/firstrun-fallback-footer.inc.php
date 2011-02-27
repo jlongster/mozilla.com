69436c1c9eb909fc9d640d1e4b7f43cac74a34ae
pascal.chevrel@mozilla-europe.org <pascal.chevrel@mozilla-europe.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-02-27 22:20:17 +0000

    l10n: css styling with transitions for fx4 firstrun page
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@83377 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/l10n/4/firstrun-fallback-footer.inc.php
+++ includes/l10n/4/firstrun-fallback-footer.inc.php
@@ -1,4 +1,11 @@
     </div> <!-- wrapper -->
 </div> <!-- doc -->
+
+    <script>
+        window.onload = function() {
+            document.getElementsByTagName('body')[0].className = 'go';
+        };
+    </script>
+
 </body>
 </html>
