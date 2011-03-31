565f351573142dcbac992f441af2113a97320af4
pascal.chevrel@mozilla-europe.org <pascal.chevrel@mozilla-europe.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-03-31 10:28:58 +0000

    l10n: don't propose Firefox 4 anywhere to Mac 10.4/PPC users, for whatsnew pages we propose 3.6 for the main download page we just hide the download box
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@86859 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/l10n/dlbox.inc.php
+++ includes/l10n/dlbox.inc.php
@@ -2,7 +2,7 @@
 
 include_once $config['file_root'].'/includes/l10n/class.novadownload.php';
 
-$dl_box_id = '';
+$dl_box_id   = 'default_l10n_download_box';
 $dl_fallback = false;
 
 // check if we have a locale remapped to another, preserve the download box link
@@ -15,13 +15,13 @@ if(isset($dl_lang)) {
 switch($pageid) {
     case 'firefox-features':
         $dl_box_class   = 'top-right';
-        $dl_box_id      = ' id="download"';
+        $dl_box_id      = 'download';
         $dl_box_options = array('layout' => 'smallbox', 'download_parent_override' => 'download');
         break;
 
     case 'oldversion':
         $dl_box_class   = 'home-download';
-        $dl_box_id      = ' id="dl_latest"';
+        $dl_box_id      = 'dl_latest';
         $dl_box_options = array();
         $dl_box_options = array('download_parent_override' => 'main-content', 'wording' => 'Firefox 4');
         $dl_fallback    = true;
@@ -51,9 +51,20 @@ $downloadbox  = "\n".'<!-- generated box -->'."\n";
 $downloadbox .= "\n".'<script type="text/javascript">//<![CDATA['."\n";
 $downloadbox .= file_get_contents($_SERVER['DOCUMENT_ROOT'].'/js/download.js');
 $downloadbox .= "\n".'//]]>></script>'."\n";
-$downloadbox .= "\n".'<div class="'.$dl_box_class.'"'.$dl_box_id.'>'."\n";
+$downloadbox .= "\n".'<div class="'.$dl_box_class.'" id="'.$dl_box_id.'">'."\n";
 $downloadbox .= $firefoxDetailsl10n->getLocaleBoxHome(localeConvert($templang), $dl_box_options);
 $downloadbox .= "\n".'</div>'."\n";
+$downloadbox .= <<<HIDE
+    <script type="text/javascript">//<![CDATA[
+      // bug 644255, don't tell users to upgrade to fx4 if they
+      // are PPC or using any OS X before 10.5
+
+    if(/(PPC|Mac OS X 10.[0-4])/.test(navigator.userAgent)) {
+        document.getElementById('{$dl_box_id}').style.display = 'none'; id="'.$dl_box_id.'"
+    }
+
+    </script>
+HIDE;
 $downloadbox .= "\n".'<!-- end generated box -->'."\n";
 
 // the following hack is for Mac 10.4 that we no longer support with Firefox 4, we need another set of 3.6 boxes for those users
@@ -67,7 +78,10 @@ if($dl_fallback == true) {
 
 $downloadbox .= <<<HIDE
     <script type="text/javascript">//<![CDATA[
-    if (gPlatform == 3 || gPlatform == 4) {
+      // bug 644255, don't tell users to upgrade to fx4 if they
+      // are PPC or using any OS X before 10.5
+
+    if(/(PPC|Mac OS X 10.[0-4])/.test(navigator.userAgent)) {
         document.getElementById('dl_latest').style.display = 'none';
     } else {
         document.getElementById('dl_fallback').style.display = 'none';
