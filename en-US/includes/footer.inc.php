c9aa483012b7750c117fa696a43d7bda67c95c44
jlong@mozilla.com <jlong@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-03-31 18:31:13 +0000

    begin porting newsletter signup block
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@86895 4eb1ac78-321c-0410-a911-ec516a8615a5

--- en-US/includes/footer.inc.php
+++ en-US/includes/footer.inc.php
@@ -5,9 +5,4 @@ $dynamic_bottom_menu = $GLOBALS['menu']->getMenuString('footer-menu');
 // Include the global footer.  All locales will include this.
 require "{$config['file_root']}/includes/footer.inc.php";
 
-// Now built in the global footer for consistency
-echo $dynamic_footer;
-
-unset($dynamic_footer);
-
 unset($dynamic_bottom_menu);
