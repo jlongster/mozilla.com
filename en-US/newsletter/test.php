8eb593fbbbdd981753c217e0864704d8f9937d29
jlong@mozilla.com <jlong@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-04-06 17:46:04 +0000

    test soap again
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@87045 4eb1ac78-321c-0410-a911-ec516a8615a5

--- en-US/newsletter/test.php
+++ en-US/newsletter/test.php
@@ -1,10 +1,15 @@
 <?php
 
-// Test for SoapClient support
-$cl = new SoapClient("http://responsys.com/webservices/services/ResponsysWSService");
-$cl->login(array("username" => "user",
-                 "password" => "12345"));
+try {
+    // Test for SoapClient support
+    $cl = new SoapClient("http://responsys.com/webservices/services/ResponsysWSService");
+    $cl->login(array("username" => "user",
+                     "password" => "12345"));
 
-var_dump($cl->__getLastRequestHeaders());
+    var_dump($cl->__getLastRequestHeaders());
+}
+catch(Exception $e) {
+    var_dump($e);
+}
 
 ?>
