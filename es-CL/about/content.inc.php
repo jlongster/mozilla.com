2c68609af5f770afcefa27b7dfbbd877a0e1eb85
djfunkinmixer@gmail.com <djfunkinmixer@gmail.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-04-25 01:22:18 +0000

    Updates in es-CL pages
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@87761 4eb1ac78-321c-0410-a911-ec516a8615a5

--- es-CL/about/content.inc.php
+++ es-CL/about/content.inc.php
@@ -1,20 +1,18 @@
 <div id="content">
-    <h1 id="page-title">About Us</h1>
+    <h1 id="page-title">Quienes somos</h1>
 
     <div class="intro">
-        <p><strong>We're a global community</strong>
-            of <a href="http://www.mozilla.org/community">people working together</a> since 1998 to build a better Internet.</p>
+        <p><strong>Somos una comunidad global</strong>
+            de <a href="http://www.mozilla.org/community">personas trabajando juntas</a> desde 1998 para hacer una mejor Internet.</p>
       
-        <p><strong>We're a non-profit organization</strong>
-            dedicated to promoting <a href="http://www.mozilla.org/about/mission.html">openness, innovation and opportunity</a> online.</p>
+        <p><strong>Somos una organización son fines de lucro</strong>
+            dedicada a promover <a href="http://www.mozilla.org/about/mission.html">la apertura, innovación y  las oportunidades</a> en línea.</p>
       
-        <p><strong>And we're an open source project</strong>
-            whose <a href="http://www.mozilla.org/projects">software</a> has been used to build some of the Internet's best applications.</p>
+        <p><strong>y somos un proyecto de código abierto</strong>
+            cuyo <a href="http://www.mozilla.org/projects">software</a> ha sido usado para crear algunas de las mejores aplicaciones de Internet.</p>
     </div>
 
-    <p>We believe that, as the most significant social and technological development of our time, the Internet is a public resource that must be improved and 
-    protected.</p>
+    <p>Creemos que, como el desarrollo más significativo, tanto social como tecnológico de nuestro tiempo, la Internet es un recurso público que debe ser mejorado y protegido.</p>
 
-    <p>The <a href="http://www.mozilla.org/foundation">Mozilla Foundation</a> is a non-profit organization dedicated to these goals, but the real force behind Mozilla is people 
-    around the world taking action to build the web they want. <a href="http://www.mozilla.org/contribute">And you can help</a>!</p>
+    <p>La <a href="http://www.mozilla.org/foundation">Fundación Mozilla</a> es una organización son fines de lucro dedicada a esas metas, pero la verdadera fuerza detrás de Mozilla es la gente de todo el mundo moviéndose para crear la Web que quieren. ¡<a href="http://www.mozilla.org/contribute">y tú puedes ayudar</a>!</p>
 </div>
