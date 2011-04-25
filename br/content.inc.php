d084271c4c893232058b27ca03431d89d742b4fe
mnedeleg@yahoo.fr <mnedeleg@yahoo.fr@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-04-25 17:12:25 +0000

    Bug 652068 - [br] New about pages on mozilla.com
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@87770 4eb1ac78-321c-0410-a911-ec516a8615a5

--- br/content.inc.php
+++ br/content.inc.php
@@ -1,20 +1,20 @@
 <div id="content">
     <h1 id="page-title">About Us</h1>
+    <h1 id="page-title">Diwar hor penn</h1>
 
     <div class="intro">
-        <p><strong>We're a global community</strong>
-            of <a href="http://www.mozilla.org/community">people working together</a> since 1998 to build a better Internet.</p>
+        <p><strong>Ur gumuniezh vedel omp</strong>
+            <a href="http://www.mozilla.org/community">o labourat</a>abaoe 1998 a-benn sevel ur gwellañ Internet.</p>
       
-        <p><strong>We're a non-profit organization</strong>
-            dedicated to promoting <a href="http://www.mozilla.org/about/mission.html">openness, innovation and opportunity</a> online.</p>
+        <p><strong>Un aozadur hep pal arc'hantus omp</strong>
+            ha gouestlet omp da gas war-raok <a href="http://www.mozilla.org/about/mission.html">an digoradur, an nevezadenn hag an diaweladoù</a> war ar rouedad.</p>
       
-        <p><strong>And we're an open source project</strong>
-            whose <a href="http://www.mozilla.org/projects">software</a> has been used to build some of the Internet's best applications.</p>
+        <p><strong>Hag ur raktres gant e darzhioù digor omp </strong>
+            hag a zo e <a href="http://www.mozilla.org/projects">veziantoù</a> arveret a-benn sevel arloadoù e-touez ar re wellañ an Internet.</p>
     </div>
 
-    <p>We believe that, as the most significant social and technological development of our time, the Internet is a public resource that must be improved and 
-    protected.</p>
+    <p>Ni a gred ez eo an Internet an diorroadur kalvezoniezhel ha kevredadel pouezusañ an amzer a-vremañ hag ul loaz foran hag a rank bezañ gwellaet ha gwarezet.</p>
 
-    <p>The <a href="http://www.mozilla.org/foundation">Mozilla Foundation</a> is a non-profit organization dedicated to these goals, but the real force behind Mozilla is people 
-    around the world taking action to build the web they want. <a href="http://www.mozilla.org/contribute">And you can help</a>!</p>
+    <p>Un aozadur hep pal arc'hantus ha gouestlet d'ar palioù-mañ eo an <a href="http://www.mozilla.org/foundation">diazezidigezh Vozilla</a> met ar greñvded wir a-dreñv Mozilla eo an dud tro-dro ar bed o labourat a-benn sevel ar web a fell dezho kaout. <a href="http://www.mozilla.org/contribute">Ha gallout a rit skoazellañ !</a>!</p>
+
 </div>
