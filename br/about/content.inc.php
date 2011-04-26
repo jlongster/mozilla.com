eab4f0f599f73faced4db0d4a3a8404cffba992f
mnedeleg@yahoo.fr <mnedeleg@yahoo.fr@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-04-26 19:41:46 +0000

    Fixes for some typo errors
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@87838 4eb1ac78-321c-0410-a911-ec516a8615a5

--- br/about/content.inc.php
+++ br/about/content.inc.php
@@ -1,10 +1,9 @@
 <div id="content">
-    <h1 id="page-title">About Us</h1>
     <h1 id="page-title">Diwar hor penn</h1>
 
     <div class="intro">
         <p><strong>Ur gumuniezh vedel omp</strong>
-            <a href="http://www.mozilla.org/community">o labourat</a>abaoe 1998 a-benn sevel ur gwellañ Internet.</p>
+            <a href="http://www.mozilla.org/community">o labourat</a> abaoe 1998 a-benn sevel ur gwellañ Internet.</p>
       
         <p><strong>Un aozadur hep pal arc'hantus omp</strong>
             ha gouestlet omp da gas war-raok <a href="http://www.mozilla.org/about/mission.html">an digoradur, an nevezadenn hag an diaweladoù</a> war ar rouedad.</p>
@@ -15,6 +14,6 @@
 
     <p>Ni a gred ez eo an Internet an diorroadur kalvezoniezhel ha kevredadel pouezusañ an amzer a-vremañ hag ul loaz foran hag a rank bezañ gwellaet ha gwarezet.</p>
 
-    <p>Un aozadur hep pal arc'hantus ha gouestlet d'ar palioù-mañ eo an <a href="http://www.mozilla.org/foundation">diazezidigezh Vozilla</a> met ar greñvded wir a-dreñv Mozilla eo an dud tro-dro ar bed o labourat a-benn sevel ar web a fell dezho kaout. <a href="http://www.mozilla.org/contribute">Ha gallout a rit skoazellañ !</a>!</p>
+    <p>Un aozadur hep pal arc'hantus ha gouestlet d'ar palioù-mañ eo an <a href="http://www.mozilla.org/foundation">diazezidigezh Vozilla</a> met ar greñvded wir a-dreñv Mozilla eo an dud tro-dro ar bed o labourat a-benn sevel ar web a fell dezho kaout. <a href="http://www.mozilla.org/contribute">Ha gallout a rit skoazellañ</a>&nbsp;!</p>
 
 </div>
