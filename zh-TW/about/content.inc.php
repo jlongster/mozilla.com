6c782b5d520b716785e9fd73fb4024a68785fa66
petercpg@mail.moztw.org <petercpg@mail.moztw.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-05-02 05:36:00 +0000

    Update About Us page
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@88100 4eb1ac78-321c-0410-a911-ec516a8615a5

--- zh-TW/about/content.inc.php
+++ zh-TW/about/content.inc.php
@@ -1,20 +1,18 @@
 <div id="content">
-    <h1 id="page-title">About Us</h1>
+    <h1 id="page-title">關於我們</h1>
 
     <div class="intro">
-        <p><strong>We're a global community</strong>
-            of <a href="http://www.mozilla.org/community">people working together</a> since 1998 to build a better Internet.</p>
+        <p><strong>我們是個全球社群</strong>
+            自 1998 年開始 <a href="http://www.mozilla.org/community">人們一起努力工作</a> 以打造更好的網際網路。<p>
       
-        <p><strong>We're a non-profit organization</strong>
-            dedicated to promoting <a href="http://www.mozilla.org/about/mission.html">openness, innovation and opportunity</a> online.</p>
+        <p><strong>我們是個非營利組織</strong>
+            致力於促進網路的 <a href="http://www.mozilla.org/about/mission.html">開放、創新，以及機會</a>。</p>
       
-        <p><strong>And we're an open source project</strong>
-            whose <a href="http://www.mozilla.org/projects">software</a> has been used to build some of the Internet's best applications.</p>
+        <p><strong>我們也是個開放原始碼計畫</strong>
+            我們的 <a href="http://www.mozilla.org/projects">軟體</a> 也被用來打造網路上一些最棒的應用程式。</p>
     </div>
 
-    <p>We believe that, as the most significant social and technological development of our time, the Internet is a public resource that must be improved and 
-    protected.</p>
+    <p>我們相信，作為我們的時代當中最重要的社交與技術發展工具，網際網路是一個應該要被持續改進並保護的公共資源。</p>
 
-    <p>The <a href="http://www.mozilla.org/foundation">Mozilla Foundation</a> is a non-profit organization dedicated to these goals, but the real force behind Mozilla is people 
-    around the world taking action to build the web they want. <a href="http://www.mozilla.org/contribute">And you can help</a>!</p>
+    <p><a href="http://www.mozilla.org/foundation">Mozilla 基金會</a> 是一個致力於這些目標的非營利性組織，但 Mozilla 背後最真實的動力來源是全世界的人們採取行動，把網路打造成他們所想要的樣子。<a href="http://www.mozilla.org/contribute">您也可以加入幫忙</a>!</p>
 </div>
