929bd1eb0941ca9149360c08f7a74f48d35dcdbc
guillermo.movia@gmail.com <guillermo.movia@gmail.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-05-03 16:11:29 +0000

    changes to mozilla.com/about page
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@88196 4eb1ac78-321c-0410-a911-ec516a8615a5

--- es-AR/about/content.inc.php
+++ es-AR/about/content.inc.php
@@ -8,11 +8,11 @@
         <p><strong>Somos una organización sin fines de lucro</strong>
             dedicada a promover <a href="http://www.mozilla.org/about/mission.html">la apertura, innovación y oportunidad</a> online.</p>
       
-        <p><strong>Y somos un proyecto de código abierto</strong>
+        <p><strong>Y somos un proyecto de software libre y código abierto</strong>
             cuyo <a href="http://www.mozilla.org/projects">software</a> ha sido usado para crear algunas de las mejores aplicaciones de Internet.</p>
     </div>
 
     <p>Creemos que, como el desarrollo más significativo tanto social como tecnológico de nuestro tiempo, Internet es un recurso público que debe ser mejorado y protegido.</p>
 
-    <p>La <a href="http://www.mozilla.org/foundation">Mozilla Foundation</a> es una organización sin fines de lucro dedicada a estos objetivos, pero la fuerza real detrás de Mozilla es las personas que alrededor del mundo trabajan para crear la web que quieren. ¡<a href="http://www.mozilla.org/contribute">Y vos podés ayudar</a>!</p>
+    <p><a href="http://www.mozilla.org/foundation">Mozilla Foundation</a> es una organización sin fines de lucro dedicada a estos objetivos, pero la fuerza real detrás de Mozilla es las personas que alrededor del mundo trabajan para crear la web que quieren. ¡<a href="http://www.mozilla.org/contribute">Y vos podés ayudar</a>!</p>
 </div>
