28403ad99365d6f2765f98dca7c5ff36cff8dd90
jlong@mozilla.com <jlong@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-05-17 20:21:56 +0000

    polish up mobile channel page, add blog feed (bug 653332)
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@88937 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ includes/feeds/rapidrelease.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once $config['file_root'].'/includes/Cache/Lite.php';
+
+class RapidReleaseFeed {
+
+    public function get_items($max_items = 4) {
+        $uri = 'http://blog.mozilla.com/futurereleases/feed/';
+        $lifetime = 450; // 7.5 mins, about half static cache time
+
+        $document = new DOMDocument();
+        $items = array();
+
+        $cache = new Cache_Lite(
+                                array(
+                                      'cacheDir' => dirname(__FILE__) . '/../../../cache/',
+                                      'lifeTime' => $lifetime,
+                                      )
+                                );
+
+        if (($xml = $cache->get($uri)) === false) {
+            $xml = file_get_contents($uri);
+            if ($xml === false) {
+                // try invalid cached version if it exists
+                if (($xml = $cache->get($uri, 'default', true)) === false) {
+                    $xml = '';
+                }
+            } else {
+                $cache->save($xml, $uri);
+            }
+        }
+
+        if ($xml != '') {
+            $document->loadXML($xml);
+            $xpath = new DOMXPath($document);
+            $item_els = $xpath->query('//item');
+            $count = 0;
+            foreach ($item_els as $item_el) {
+                $item = array();
+                $item['title'] = $xpath->evaluate('string(title/text())', $item_el);
+                $item['link'] = $xpath->evaluate('string(link/text())', $item_el);
+                $item['date'] = strtotime($xpath->evaluate('string(pubDate/text())', $item_el));
+                $categories = array();
+                $category_els = $xpath->query('category/text()', $item_el);
+                foreach ($category_els as $category) {
+                    $categories[] = $category->data;
+                }
+                $item['categories'] = $categories;
+                $items[] = $item;
+                $count++;
+
+                if ($count >= $max_items) {
+                    break;
+                }
+            }
+        }
+
+        return $items;
+    }
+}
+
+?>
