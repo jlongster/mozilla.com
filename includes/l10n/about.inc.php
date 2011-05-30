b3b81facf5b89a5a6245eea9a58a9d312ffecfc2
pascal.chevrel@mozilla-europe.org <pascal.chevrel@mozilla-europe.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-05-30 16:13:31 +0000

    l10n: clean up about page code
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@89596 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/l10n/about.inc.php
+++ includes/l10n/about.inc.php
@@ -1,16 +1,23 @@
 <?php
 
-$body_id    = 'firefox-about';
+$body_id = 'firefox-about';
 
-if(!isset($meta_description)) {$meta_description = '';}
-if(!isset($extra_headers)) {$extra_headers = '';}
-
+/*
+    Page specific CSS
+*/
 
 $extra_headers .= <<<EXTRA_HEADERS
     <link rel="stylesheet" type="text/css" href="{$config['static_prefix']}/style/covehead/about.css" media="screen" />
+EXTRA_HEADERS;
+
+/*
+    locales style mods for the page
+*/
+
+$extra_headers .= <<<EXTRA_HEADERS
     <style>
     #wrapper {
-        background: url("/img/covehead/firefox/survey/thanks-background.png") no-repeat scroll 1100px 150px transparent;
+        background: url("{$config['static_prefix']}/img/covehead/firefox/survey/thanks-background.png") no-repeat scroll 1100px 150px transparent;
     }
 
     #main-feature, #content {
@@ -18,25 +25,51 @@ $extra_headers .= <<<EXTRA_HEADERS
         font-size: 16px;
     }
 
+    </style>
+EXTRA_HEADERS;
+
+/*
+    RTL Support
+*/
+
+if ($textdir == 'rtl') {
+    $extra_headers .= <<<RTL
+    <style>
+        
     body.rtl #wrapper {
-        background: url("/img/covehead/firefox/survey/thanks-background-rtl.png") no-repeat scroll 200px 150px transparent;
+        background: url("{$config['static_prefix']}/img/covehead/firefox/survey/thanks-background-rtl.png") no-repeat scroll 200px 150px transparent !important;
     }
 
 
     body.rtl #content,
-    body.rtl #main-feature
-    {
+    body.rtl #main-feature {
         margin-left: 400px !important;
         margin-right: 20px !important;
     }
 
-
-
-
     </style>
-EXTRA_HEADERS;
+RTL;
+}
 
-include_once "{$config['file_root']}/includes/l10n/header-pages.inc.php";
-include_once "{$config['file_root']}/{$lang}/about/content.inc.php";
-include_once "{$config['file_root']}/includes/l10n/footer-pages.inc.php";
-?>
+
+/*
+    build the page
+*/
+
+require_once $config['file_root'] . '/includes/l10n/header-pages.inc.php';
+
+/*
+* We shouldn't have used content.inc.php as a ftile name but content.inc.html
+* remove the check once we have all the files translated and published as content.inc.html
+*/
+
+$translation = $config['file_root'] . '/' . $lang . '/about/content.inc.html';
+
+if (file_exists($translation)) {
+    include_once $translation;
+} else {
+    include_once $config['file_root'] . '/' . $lang . '/about/content.inc.php';
+}
+
+require_once $config['file_root'] . '/includes/l10n/footer-pages.inc.php';
+
