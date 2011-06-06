27b282de0f242204a496fc4ca94abd6f0b317374
pascal.chevrel@mozilla-europe.org <pascal.chevrel@mozilla-europe.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-06-06 18:41:00 +0000

    l10n: add an automatic test to check if the locales we have in product-details map correctly the locales we have built in shipped-locales on hg
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@89994 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ includes/l10n/tests/testLocalesReleased.php
@@ -0,0 +1,83 @@
+<?php
+
+function secureText($var, $tablo = true) {
+    if (!is_array($var)) {
+        $var   = array($var);
+        $tablo = false;
+    }
+
+    foreach ($var as $item => $value) {
+        // CRLF XSS
+        $item  = str_replace('%0D', '', $item);
+        $item  = str_replace('%0A', '', $item);
+        $value = str_replace('%0D', '', $value);
+        $value = str_replace('%0A', '', $value);
+        $item  = strip_tags($item);
+        $value = strip_tags($value);
+
+        // Repopulate value
+        $var2[$item] = $value;
+    }
+
+    return ($tablo == true) ? $var2 : $var2[0];
+}
+
+function getLocales($array1, $array2) {
+    $tmp   = array_diff($array1, $array2);
+    $value = '';
+
+    foreach ($tmp as $val) {
+        $value .=  $val . ', ';
+    }
+
+    return $value;
+}
+
+// Load the product details classes
+require dirname(__FILE__).'/../../product-details/firefoxDetails.class.php';
+$fx = new firefoxDetails();
+$pd = array();
+
+if ( isset($_GET['fx']) && isset($_GET['hg']) && isset($_GET['type']) ) {
+    $release   = secureText($_GET['fx']);
+    $changeset = secureText($_GET['hg']);
+    $type      = secureText($_GET['type']);
+    $hg  = 'http://hg.mozilla.org/releases/' . $type . '/raw-file/' . $changeset . '/browser/locales/shipped-locales';
+} else {
+    exit;
+}
+
+$file = file($hg, FILE_IGNORE_NEW_LINES);
+
+if (in_array('ja linux win32', $file) && in_array('ja-JP-mac osx', $file) ) {
+    unset($file[array_search('ja linux win32', $file)]);
+    unset($file[array_search('ja-JP-mac osx', $file)]);
+    $file[] = 'ja';
+    asort($file);
+    $file = array_values($file);
+}
+
+foreach($fx->primary_builds as $key => $val) {
+
+    if(isset($val[$release])) {
+        $pd[] = $key;
+    }
+}
+
+foreach($fx->beta_builds as $key => $val) {
+
+    if(isset($val[$release])) {
+        $pd[] = $key;
+    }
+}
+
+echo 'RELEASE:' . $release . '<br/>';
+echo '<a href="' . $hg . '">HG source</a><br/>';
+
+echo '<p>Locales we have built but are not proposed on the website:</p>';
+
+echo getLocales($file, $pd);
+
+echo '<p>Locales we propose on the website but don\'t have builds for:</p>';
+
+echo getLocales($pd, $file);
