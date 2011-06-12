1327c6c3f67eb9c2577f93284079c5a6a6e08f99
mdinic@mozilla.com <mdinic@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-06-12 16:25:01 +0000

    Bug 651418: New about pages in Serbian; p=vladimir, r=milos
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@90377 4eb1ac78-321c-0410-a911-ec516a8615a5

--- sr/about/content.inc.php
+++ sr/about/content.inc.php
@@ -1,20 +1,19 @@
 <div id="content">
-    <h1 id="page-title">About Us</h1>
+    <h1 id="page-title">О нама</h1>
 
     <div class="intro">
-        <p><strong>We're a global community</strong>
-            of <a href="http://www.mozilla.org/community">people working together</a> since 1998 to build a better Internet.</p>
+        <p><strong>Ми смо глобална заједница</strong>
+            <a href="http://www.mozilla.org/community">људи који раде заједно</a> још од 1998. године на стварању бољег интернета.</p>
       
-        <p><strong>We're a non-profit organization</strong>
-            dedicated to promoting <a href="http://www.mozilla.org/about/mission.html">openness, innovation and opportunity</a> online.</p>
+        <p><strong>Ми смо непрофитна организација</strong>
+            посвећена промовисању <a href="http://www.mozilla.org/about/mission.html">отворености, иновативности и приликама</a> на мрежи.</p>
       
-        <p><strong>And we're an open source project</strong>
-            whose <a href="http://www.mozilla.org/projects">software</a> has been used to build some of the Internet's best applications.</p>
+        <p><strong>Осим тога, ми смо пројекат отвореног кôда</strong>
+            чији <a href="http://www.mozilla.org/projects">софтвер</a> се користи за прављење неких од најбољих апликација на интернету.</p>
     </div>
 
-    <p>We believe that, as the most significant social and technological development of our time, the Internet is a public resource that must be improved and 
-    protected.</p>
+    <p>Верујемо да је интернет јавно добро које, као најважнији део социјалног и технолошког развоја нашег времена, мора бити побољшано и заштићено.</p>
 
-    <p>The <a href="http://www.mozilla.org/foundation">Mozilla Foundation</a> is a non-profit organization dedicated to these goals, but the real force behind Mozilla is people 
-    around the world taking action to build the web they want. <a href="http://www.mozilla.org/contribute">And you can help</a>!</p>
+    <p><a href="http://www.mozilla.org/foundation">Задужбина Мозила</a> је непрофитна организација посвећена остваривању ових циљева, али њена права снага је у људима широм света који раде на обликовању интернета према потребама корисника. <a href="http://www.mozilla.org/contribute">И ти можеш да помогнеш</a>!</p>
 </div>
+
