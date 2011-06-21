4b5dbbb43cebebc097b6ea9d5a4189fd97ded97f
clegnitto@mozilla.com <clegnitto@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-06-21 01:35:09 +0000

    Fix sidebar for Firefox 5
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@90963 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ en-US/includes/release-notes-sidebar-5.inc.php
@@ -0,0 +1,23 @@
+<div id="sidebar">
+	<div class="sidebar-section">
+		<h4>Want to Get Involved?</h4>
+		<p>Did you know that most of the content on Firefox Support was written by volunteers?<br /><a href="http://www.mozilla.org/contribute/">Find Out More »</a></p>
+	</div>
+	<div class="sidebar-section">
+		<h4>Spread the Word!</h4>
+		<p>Fans around the world make Firefox and Mozilla a success! Take a minute to help.<br /><a href="/<?=$lang?>/about/participate/">Find Out How »</a></p>
+	</div>
+	<div class="sidebar-section">
+		<h4>Other Resources &amp; Links</h4>
+		<p>The following resources contain additional useful information about Firefox 5:</p>
+		<ul class="spaced">
+		  <li><a href="http://support.mozilla.com/">Firefox Support Page</a></li>
+		  <li><a href="http://kb.mozillazine.org/">MozillaZine's Knowledge Base</a></li>
+		  <li><a href="https://developer.mozilla.org/en/Firefox_5_for_developers">Developer Information</a></li>
+		  <li><a href="http://code.google.com/apis/safebrowsing/firefox3_privacy.html">SafeBrowsing Service
+			Privacy Policy</a> (for anti-phishing/anti-malware feature)</li>
+
+		  <li><a href="http://www.google.com/privacy-lsf.html">Google Geolocation Privacy Policy</a></li>
+		</ul>
+	</div>
+</div>
