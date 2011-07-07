622b7a2b03b98a5c698590cfe5036b4ac17db1b8
aricaud@mozilla.com <aricaud@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-07-07 08:38:06 +0000

    Bug 663458 - Redirect Firefox 7 users to the /fx/ homepage
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@92227 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/prefetch.php
+++ includes/prefetch.php
@@ -183,8 +183,7 @@ if (substr($_SERVER['REDIRECT_URL'], 1, strlen($lang)) != $lang) {
             // Bug 629407 Redirect to user-specific pages
             // This is also implemented in .htaccess, but we do it here
             // to redirect the user only once
-            if (preg_match('/Firefox\/5/', $ua) ||
-                preg_match('/Firefox\/6/', $ua)) {
+            if (preg_match('/Firefox\/(5|6|7)/', $ua)) {
                 $_SERVER['REQUEST_URI'] = '/firefox/fx/';
             }
             else {
