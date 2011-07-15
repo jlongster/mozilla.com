c09cedfd5a06e7dc94620d375a19a6fd9e3e5803
steven@silverorange.com <steven@silverorange.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-07-15 15:01:55 +0000

    Add tracking code to l10n versions of XP campaign landing page (Bug 660249)
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@92567 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/l10n/marketing/home.xpcampaign.inc.php
+++ includes/l10n/marketing/home.xpcampaign.inc.php
@@ -55,6 +55,12 @@
 
 </div>
 
-
-
-
+<!-- measure tag -->
+<IFRAME
+SRC="http://media.mozilla.com/ipixel?spacedesc=1121943_1061349_1x1_1061349_1061349&db_afcr=123&target=_blank&group=Mozilla&event=LandingPage&revenue=REVENUE&random=CACHEBUSTER"
+     WIDTH="1" HEIGHT="1" SCROLLING="No" FRAMEBORDER="0" MARGINHEIGHT="0" MARGINWIDTH="0">
+<![if lt IE 5]>
+<SCRIPT
+SRC="http://media.mozilla.com/jpixel?spacedesc=1121943_1061349_1x1_1061349_1061349&db_afcr=123&target=_blank&group=Mozilla&event=LandingPage&revenue=REVENUE&random=CACHEBUSTER"></SCRIPT>
+<![endif]>
+</IFRAME>
