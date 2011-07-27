3a6981b9d5a4a056ac81e9df3dc36927778f7904
selim@selimsumlu.com.tr <selim@selimsumlu.com.tr@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-07-27 09:10:40 +0000

    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@92981 4eb1ac78-321c-0410-a911-ec516a8615a5

--- tr/about/content.inc.php
+++ tr/about/content.inc.php
@@ -4,12 +4,12 @@
     <div class="intro">
         <p>Biz, 1998'den beri daha iyi bir internet inşa etmek etmek için <a href="http://www.mozilla.org/community">birlikte çalışan insanlardan oluşan</a> <strong>küresel bir topluluğuz</strong>.</p>
       
-        <p>Biz, kendisini, internette <a href="http://www.mozilla.org/about/mission.html">açıklık, yenilik ve fırsat eşitliğini</a> yaymaya adamış <strong>kâr amacı gütmeyen bir kuruluşuz</strong>.</p>
+        <p>Biz; kendisini; internette <a href="http://www.mozilla.org/about/mission.html">açıklık, yenilik ve fırsat eşitliğini</a> yaymaya adamış <strong>kâr amacı gütmeyen bir kuruluşuz</strong>.</p>
       
-        <p>Ve biz, <a href="http://www.mozilla.org/projects">yazılımları</a> internetin en iyi uygulamalarından bazılarına geliştirmek için kullanılan <strong>açık kaynaklı bir projeyiz</strong>.</p>
+        <p>Ve biz, <a href="http://www.mozilla.org/projects">yazılımları</a> internetin en iyi uygulamalarından bazılarını geliştirmek için kullanılan <strong>açık kaynaklı bir projeyiz</strong>.</p>
     </div>
 
-    <p>Biz, çağımızın en büyük sosyal ve teknolojik gelişmesi olan internetin geliştirilmesi ve korunması gereken kamusal bir kaynak olduğuna inanıyoruz.</p>
+    <p>Biz, çağımızın en büyük sosyal ve teknolojik gelişmesi olan internetin, geliştirilmesi ve korunması gereken kamusal bir kaynak olduğuna inanıyoruz.</p>
 
     <p><a href="http://www.mozilla.org/foundation">Mozilla Vakfı</a> bu hedefleri gerçekleştirmek için kurulmuş kâr amacı gütmeyen bir kuruluştur; ama Mozilla'nın arkasındaki gerçek güç, dünyanın her tarafında kendi hayallerindeki web'i yaratmak için çalışan insanlardır. <a href="http://www.mozilla.org/contribute">Siz de onlardan biri olabilirsiniz</a>!</p>
 </div>
