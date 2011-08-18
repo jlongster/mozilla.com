4b979936f5e38d1b5aefa44dff9f48b646e0466b
wreynolds@mozilla.com <wreynolds@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-08-18 04:54:24 +0000

    Change config file to dist, remove API key
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@93997 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ en-US/newsletter/fb/fb-config-dist.php
@@ -0,0 +1,5 @@
+<?php
+/* Only for development use. Use fb-config.php for production. */
+define("APP_ID", "");
+define("APP_SECRET", "");
+?>
--- en-US/newsletter/fb/fb-config.php
+++
@@ -1,3 +0,0 @@
-<?php
-define("APP_ID", "247890471898410");
-define("APP_SECRET", "ae6d7e86bf59487c34dce44f9495f9d3");
