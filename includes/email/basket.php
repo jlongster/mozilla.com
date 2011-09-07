9acd975ae57bcd1119996faa013808ab357962cd
jlong@mozilla.com <jlong@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-09-07 04:02:27 +0000

    hardcode basket URL to test server (bug 663528)
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@94784 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/email/basket.php
+++ includes/email/basket.php
@@ -26,7 +26,8 @@ class BasketService {
     function curl($method, $data=NULL) {
         global $config;
 
-        $_curl = curl_init("{$config['basket_url']}/news/" . $method);
+        //$_curl = curl_init("{$config['basket_url']}/news/" . $method);
+        $_curl = curl_init("http://jlongster.com:7001/news/" . $method);
         curl_setopt($_curl, CURLOPT_FOLLOWLOCATION, true);  
         curl_setopt($_curl, CURLOPT_RETURNTRANSFER, true);
 
