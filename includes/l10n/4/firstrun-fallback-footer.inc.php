735b219c3c2a3bb8ce8dbc47725f02b54a4bd5a2
pascal.chevrel@mozilla-europe.org <pascal.chevrel@mozilla-europe.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-09-08 16:19:28 +0000

    l10n: Bug 683463, add release notes, Support and features links in the footer of rapid releases fallback whatsnew pages
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@94883 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/l10n/4/firstrun-fallback-footer.inc.php
+++ includes/l10n/4/firstrun-fallback-footer.inc.php
@@ -1,3 +1,12 @@
+
+<?php if($pageid == 'whatsnew-4'): ?>
+
+<p id="sub-links">
+    <a href="/<?=$lang;?>/firefox/<?=getVersionBySelf();?>/releasenotes/"><?=___('Release Notes');?>&nbsp;<span>»</span></a>
+    <a href="/<?=$lang;?>/firefox/features/"><?=___('Features');?>&nbsp;<span>»</span></a>
+    <a href="http://support.mozilla.com"><?=___('Support');?>&nbsp;<span>»</span></a>
+</p>
+<?php endif; ?>
 </div> <!-- wrapper -->
 
     <script>
