cedb3967c6bd8cafa1038efbc65deec7acc8da58
steven@silverorange.com <steven@silverorange.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-09-20 20:04:00 +0000

    Bug 687965 - Cleaning up more unused css/img files
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@95162 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/headers/mobile/faq.php
+++ includes/headers/mobile/faq.php
@@ -5,7 +5,7 @@
 $body_id = 'mobile-faq';
 $extra_headers = <<<EXTRA_HEADERS
     <link rel="stylesheet" href="{$config['static_prefix']}/style/tignish/mobile-later.css" media="screen" />
-    <link rel="stylesheet" href="{$config['static_prefix']}/style/tignish/mozilla-expanders.css" media="screen" />
+    <link rel="stylesheet" href="{$config['static_prefix']}/style/covehead/mozilla-expanders.css" media="screen" />
     <script src="{$config['static_prefix']}/js/jquery/jquery.min.js"></script>
     <script src="{$config['static_prefix']}/js/mozilla-expanders.js"></script>
 EXTRA_HEADERS;
