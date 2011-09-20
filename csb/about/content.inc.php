121b0f141180d2ee5b2da4567fe1b4a7bb84a701
jurk@linuxcsb.org <jurk@linuxcsb.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-09-20 17:55:40 +0000

    The about folder
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@95155 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ csb/about/content.inc.php
@@ -0,0 +1,19 @@
+﻿<div id="content">
+    <h1 id="page-title">Ò nas</h1>
+
+    <div class="intro">
+        <p><strong>Jesmë swiatową spòlëzną</strong>
+            <a href="http://www.mozilla.org/community"> wespółrobiącëch lëdzy</a> chtërni òd 1998 rokù mają dbã ò lepszi Internet.</p>
+      
+        <p><strong>Nié robimë dlô dëtków</strong>
+            ë wierzimë <a href="http://www.mozilla.org/about/mission.html">w òtemkłosc, jinowacjô ë równé szansã</a> w sécë.</p>
+      
+        <p><strong>Jesmë ôpen-zdrojowim projektã</strong>
+            chtërnégò <a href="http://www.mozilla.org/projects">narzãdza</a> są ùżiwóné przë bùdowanim nôlepszich programów sécowich.</p>
+    </div>
+
+    <p>Mómë wiarã w to że jakno nôwôżnészi rozwij społeczny ë technologiczny najich czasów, Jinternet je pùblicznym dobrã chtëren mùszi bëc rozwijóny ë chróniony.</p>
+
+    <p><a href="http://www.mozilla.org/foundation">Fùndacëjô Mozilla</a> je niédochòdową òrganizacëją wspiérającą ne céle. Ale nôwikszim atutã Mozillë są lëdze z całégò swiata chtërni nadżibają séc do swòjégò brëkùnkù.
+    <a href="http://www.mozilla.org/contribute">Të téż mòżesz pòmóc</a>!</p>
+</div>
