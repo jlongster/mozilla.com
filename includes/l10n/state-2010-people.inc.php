720d00d155b08cf36f473a0b08a5a914a04521e4
pascal.chevrel@mozilla-europe.org <pascal.chevrel@mozilla-europe.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-10-01 02:57:58 +0000

    l10n: state of mozilla, sync ongoing work
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@95657 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/l10n/state-2010-people.inc.php
+++ includes/l10n/state-2010-people.inc.php
@@ -16,6 +16,26 @@ $navigation = <<<NAV
 
 NAV;
 
+$video_placeholder = <<<VIDEO
+    <div id="video-player" class="mozilla-video-control">
+        <a href="http://videos-cdn.mozilla.net/serv/webmademovies/Moz_Doc_0329_GetInvolved_ST.webm" id="video-getinvolved"><img width="400" height="225" src="{$config['static_prefix']}/img/covehead/annualreport/poster-getinvolved.jpg" alt="Video: {$l10n->get('Getting Involved')}" /></a>
+    </div>
+    <div id="video-description">
+        <h3>{$l10n->get('Getting Involved')} <span class="time">2:58</span></h3>
+        <p>{$l10n->get('Mozilla is a fun, diverse community of people from around the world.')}</p>
+
+        <p>{$l10n->get('Download this video:')}</p>
+        <ul class="download">
+            <li><a href="http://videos-cdn.mozilla.net/serv/webmademovies/Moz_Doc_0329_GetInvolved_ST.webm">{$l10n->get('WebM format')}</a></li>
+            <li><a href="http://videos-cdn.mozilla.net/serv/webmademovies/Moz_Doc_0329_GetInvolved_ST.ogv">{$l10n->get('Ogg Theora format')}</a></li>
+            <li><a href="http://videos-cdn.mozilla.net/serv/webmademovies/Moz_Doc_0329_GetInvolved_ST.mp4">{$l10n->get('MPEG-4 format')}</a></li>
+        </ul>
+    </div>
+
+VIDEO;
+
 require_once $config['file_root'].'/includes/l10n/header-annual-report-2010.inc.php';
 require_once $config['file_root'].'/'.$lang.'/foundation/annualreport/2010/people/content.inc.html';
 require_once $config['file_root'].'/includes/l10n/footer-annual-report-2010.inc.php';
+
+
