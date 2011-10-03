14166df132aa35808d282abc6d56de72d51a94dc
stephen@silverorange.com <stephen@silverorange.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-10-03 20:35:49 +0000

    updated photo for 689653 State of Mozilla
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@95773 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/l10n/state-2010-faq.inc.php
+++ includes/l10n/state-2010-faq.inc.php
@@ -35,8 +35,8 @@ require_once $config['file_root'].'/includes/l10n/header-annual-report-2010.inc.
 
             <div class="content-block">
                 <div class="img-right">
-                    <img src="<?=$config['static_prefix']?>/img/covehead/annualreport/photo-firefox-billboard.jpg" width="297" height="435" alt="<?=___('Firefox billboard, San Francisco');?>" />
-                    <p><?=___('Firefox billboard, San Francisco');?></p>
+                    <img src="<?=$config['static_prefix']?>/img/covehead/annualreport/photo-not-every-venture.jpg" width="297" height="396" alt="<?=___('Mozilla Firefox billboard, San Francsico');?>" />
+                    <p><?=___('Mozilla Firefox billboard, San Francsico');?></p>
                 </div>
 <?php
 require_once $config['file_root'].'/'.$lang.'/foundation/annualreport/2010/faq/content.inc.html';
