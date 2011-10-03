ee0aafe3093baf3e8c86d6e9aafb22c1d89e61fe
jurk@linuxcsb.org <jurk@linuxcsb.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-10-03 16:46:43 +0000

    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@95755 4eb1ac78-321c-0410-a911-ec516a8615a5

--- csb/about/content.inc.php
+++ csb/about/content.inc.php
@@ -1,19 +1,19 @@
-﻿<div id="content">
+<div id="content">
     <h1 id="page-title">Ò nas</h1>
 
     <div class="intro">
         <p><strong>Jesmë swiatową spòlëzną</strong>
-            <a href="http://www.mozilla.org/community"> wespółrobiącëch lëdzy</a> chtërni òd 1998 rokù mają dbã ò lepszi Internet.</p>
+            <a href="http://www.mozilla.org/community"> wespółrobiącëch lëdzy</a> chtërny òd 1998 rokù mają dbã ò lepszi Internet.</p>
       
-        <p><strong>Nié robimë dlô dëtków</strong>
-            ë wierzimë <a href="http://www.mozilla.org/about/mission.html">w òtemkłosc, jinowacjô ë równé szansã</a> w sécë.</p>
+        <p><strong>Nie robimë dlô dëtków</strong>
+            ë wierzimë <a href="http://www.mozilla.org/about/mission.html">w òtemkłosc, jinowacjã ë równé szanse</a> w sécë.</p>
       
         <p><strong>Jesmë ôpen-zdrojowim projektã</strong>
-            chtërnégò <a href="http://www.mozilla.org/projects">narzãdza</a> są ùżiwóné przë bùdowanim nôlepszich programów sécowich.</p>
+            chtërnégò <a href="http://www.mozilla.org/projects">nôrzãdła</a> są ùżiwóné przë bùdowanim nôlepszich programów sécowëch.</p>
     </div>
 
-    <p>Mómë wiarã w to że jakno nôwôżnészi rozwij społeczny ë technologiczny najich czasów, Jinternet je pùblicznym dobrã chtëren mùszi bëc rozwijóny ë chróniony.</p>
+    <p>Mómë wiarã w to że jakno nôwôżnészi rozwij społeczny ë technologiczny najich czasów, Internet je pùblëcznym dobrã chtërno mùszi bëc rozwijóny ë chroniony.</p>
 
-    <p><a href="http://www.mozilla.org/foundation">Fùndacëjô Mozilla</a> je niédochòdową òrganizacëją wspiérającą ne céle. Ale nôwikszim atutã Mozillë są lëdze z całégò swiata chtërni nadżibają séc do swòjégò brëkùnkù.
+    <p><a href="http://www.mozilla.org/foundation">Fùndacjô Mozilla</a> je niedochòdową òrganizacją wspiérającą ne céle. Ale nôwikszim atutã Mozillë są lëdze z całégò swiata chtërny nadżibają séc do swòjich pòtrzébnotów.
     <a href="http://www.mozilla.org/contribute">Të téż mòżesz pòmóc</a>!</p>
 </div>
