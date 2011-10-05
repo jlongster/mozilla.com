301ff11de493aeabdcdfadf5fd8df1411a993532
pascal.chevrel@mozilla-europe.org <pascal.chevrel@mozilla-europe.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-10-05 08:40:07 +0000

    l10n: udpdate /m/ langswitcher to be section agnostic to allow it to be reused on State of Mozilla pages
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@95893 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/l10n/langswitcher.inc.php
+++ includes/l10n/langswitcher.inc.php
@@ -15,13 +15,21 @@ require $config['file_root'].'/includes/l10n/toolbox.inc.php';
 
 
 if(!isset($_REQUEST['flang']) OR !in_array($_REQUEST['flang'], $full_languages)) {
-    noCachingRedirect('http://www.mozilla.com/');
+    noCachingRedirect('http://www.mozilla.org/firefox/');
     exit;
 }
 
+// security measure for potential CRLF attacks
+$_REQUEST['flang'] = str_replace("%0D", "", $_REQUEST['flang']);
+$_REQUEST['flang'] = str_replace("%0A", "", $_REQUEST['flang']);
+echo '<pre>';
+// sanitize the value in HTTP_REFERER to control the redirect by removing the domain, url scheme and locale
+$destination = str_replace('http://',  '', $_SERVER['HTTP_REFERER']);
+$destination = str_replace('https://', '', $destination);
+$destination = explode('/', $destination);
+$destination = array_splice($destination, 2);
+$destination = implode('/', $destination);
+$destination = $config['url_scheme'] . '://' . $config['server_name'] . '/' . $_REQUEST['flang'] . '/' . $destination;
 
-$_REQUEST['flang'] = str_replace("%0D", "", $_REQUEST['flang']); // security measure for potential CRLF attacks
-$_REQUEST['flang'] = str_replace("%0A", "", $_REQUEST['flang']); // security measure for potential CRLF attacks
-
-permanentRedirect($config['url_scheme'].'://'.$config['server_name'].'/'.$_REQUEST['flang'].'/m/');
+permanentRedirect($destination);
 exit;
