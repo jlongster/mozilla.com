3914a6071da7dd65ef87af7e52c273ea4fc6f6bc
steven@silverorange.com <steven@silverorange.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-10-31 15:45:20 +0000

    Bug 697510 - Add Webtrends Tags to All Product Pages (currently only on en-US)
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@96981 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/l10n/4/firstrun-fallback-footer.inc.php
+++ includes/l10n/4/firstrun-fallback-footer.inc.php
@@ -19,5 +19,10 @@
         };
     </script>
 
+    <?
+    // Webtrends stats, see bug 556384
+    require "{$config['file_root']}/includes/js_stats.inc.php";
+    echo $stats_js;
+    ?>
 </body>
 </html>
--- includes/l10n/4/footer.inc.php
+++ includes/l10n/4/footer.inc.php
@@ -1,2 +1,8 @@
+    </script>
+    <?
+    // Webtrends stats, see bug 556384
+    require "{$config['file_root']}/includes/js_stats.inc.php";
+    echo $stats_js;
+    ?>
 </body>
 </html>
