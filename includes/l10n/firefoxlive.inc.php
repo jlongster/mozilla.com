2c9e62284d3f6737a00304e32d7a92d58b584304
steven@silverorange.com <steven@silverorange.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-11-01 00:10:13 +0000

    Add overlays, pre-video animation, cookies, etc to Firefox Live 2 (Bug 697886)
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@97000 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/l10n/firefoxlive.inc.php
+++ includes/l10n/firefoxlive.inc.php
@@ -62,7 +62,16 @@ $button_zoo     = '<img src="'.$config['static_prefix'].'/img/covehead/firefoxli
 
 $video_code =  <<<VIDEO_CODE
 <div id="video">
-<script src="http://player.ooyala.com/player.js?width=444&height=334&embedCode=s0MmVvMTrSlB1ZLzaWXnKZaa42Ib5rJV"></script>
+<script src="http://player.ooyala.com/player.js?width=444&height=334&wmode=opaque&embedCode=s0MmVvMTrSlB1ZLzaWXnKZaa42Ib5rJV"></script>
+</div>
+<div id="video-controls">
+    <h4>Cub Cam</h4>
+    <ul>
+        <li><a href="#">360° Cute</a></li>
+        <li><a href="#">Frolic</a></li>
+        <li><a href="#">Snuggle</a></li>
+        <li><a href="#faq" id="overlay-open-faq">View FAQ</a></li>
+    </ul>
 </div>
 VIDEO_CODE;
 
@@ -125,7 +134,7 @@ VIDEO_CODE;
             <a href="http://www.mozilla.org/" class="mozilla" title="<?=str_replace('.com', '.org', $l10n->get('Visit Mozilla.com'));?>">Mozilla</a>
         </div>
 
-        <h1 id="site-title"><img src="<?=$config['static_prefix']?>/img/covehead/firefoxlive/title.png" height="94" alt="<?=$l10n->get('Firefox Live')?>" /></h1>
+        <h1 id="site-title"><img src="<?=$config['static_prefix']?>/img/covehead/firefoxlive/title-logo.png" height="94" alt="" /><img src="<?=$config['static_prefix']?>/img/covehead/firefoxlive/title-en-US.png" height="94" alt="<?=$l10n->get('Firefox Live')?>" /></h1>
 
     </header>
 
@@ -183,6 +192,372 @@ require "{$config['file_root']}/includes/js_stats.inc.php";
     <!-- end #footer -->
     <?=$stats_js?>
     <?=$extra_footers?>
+
+    <script>
+    // <![CDATA[
+
+    var isIE6 = ($.browser.msie && parseInt($.browser.version, 10) <= 6);
+    var isFirefox = (/\sFirefox/.test(window.navigator.userAgent));
+
+    var close_text = '<?=$l10n->get('Close')?>';
+
+    var setCookie = function(name, value, path, expire)
+    {
+        if (expire) {
+            var expdate = new Date();
+            expdate.setDate(expdate.getDate() + expire);
+            expire = ';expires=' + expdate.toUTCString();
+        } else {
+            expire = '';
+        }
+
+        if (path) {
+            path = ';path=' + path;
+        } else {
+            path = '';
+        }
+
+        document.cookie = name + '=' + escape(value) + expire + path;
+    };
+
+    var getCookie = function(name)
+    {
+        if (document.cookie.length === 0) {
+            return null;
+        }
+
+        var start = document.cookie.indexOf(name + '=');
+
+        if (start === -1) {
+            return null;
+        }
+
+        start += name.length + 1;
+        end    = document.cookie.indexOf(';', start);
+        if (end === -1) {
+            end = document.cookie.length;
+        }
+
+        return unescape(document.cookie.substring(start, end));
+    };
+
+    var Overlay = function(selector)
+    {
+        var that = this;
+        this.selector = selector;
+        this.opened = false;
+        $(document).ready(function() {
+            that.init();
+        });
+        Overlay.overlays.push(this);
+    };
+
+    Overlay.overlays = [];
+    Overlay.openCount = 0;
+
+    Overlay.prototype.init = function()
+    {
+        var that = this;
+
+        this.$overlay = $('<a class="overlay" href="#close" />')
+            .hide()
+            .appendTo('body')
+            .click(function(e) { e.preventDefault(); that.close(); });
+
+        this.$el = $('<div class="overlay-window"></div>')
+            .appendTo('body');
+
+        var $content = $(this.selector);
+        $content
+            .appendTo(this.$el)
+            .css('display', 'block');
+
+        this.$el.append(
+            $('<div class="overlay-close" />').append(
+                $('<a href="#close" />').click(function(e) {
+                    e.preventDefault();
+                    that.close();
+                }).append(
+                    $('<img src="/img/covehead/video/clothes-lol.png" height="32" width="32" alt="' + close_text + '" />')
+                )
+            )
+        );
+            console.log('inited' + this.selector);
+    };
+
+    Overlay.prototype.open = function()
+    {
+        // hide the language form because its select element won't render
+        // correctly in IE6
+        $('#lang_form').hide();
+
+        if (isIE6) {
+            this.$el.css('position', 'absolute');
+            this.$overlay.css('position', 'absolute');
+        }
+
+        var that = this;
+        this.resizeHandler = function(e) {
+            that.resize();
+        };
+
+        $(window).resize(this.resizeHandler);
+
+        if (isIE6) {
+            $(window).scroll(this.resizeHandler);
+        }
+
+        this.$el
+            .css('visibility', 'hidden')
+            .show();
+
+        this.height = this.$el.height();
+
+        this.resize();
+
+        this.$el
+            .hide()
+            .css('visibility', 'visible');
+
+        this.$overlay.fadeTo(400, 0.75);
+        this.$el.fadeIn();
+
+        this.opened = true;
+        Overlay.openCount++;
+        Slideshow.pause();
+    };
+
+    Overlay.prototype.resize = function()
+    {
+        var width  = this.$el.width();
+        var height = this.$el.height();
+
+        if (isIE6) {
+            var scrollTop = $(window).scrollTop();
+            var docHeight = $(document).height();
+            var winHeight = $(window).height();
+            var bottom = scrollTop + (winHeight + playerHeight) / 2;
+            if (bottom > docHeight) {
+                // this prevents infinite scroll
+                this.$el.css('top', docHeight - height - 10);
+            } else {
+                this.$el.css('top', scrollTop + (winHeight - height) / 2);
+            }
+            this.$el.css('margin-left', -(width / 2));
+            this.$overlay.height(docHeight);
+        } else {
+
+            this.$el
+                .css('top', ($(window).height() - height) / 2)
+                .css('margin-left', -(width / 2));
+
+            this.$overlay.height($(window).height());
+        }
+    };
+
+    Overlay.prototype.close = function()
+    {
+        this.$overlay.fadeOut();
+        this.$el.fadeOut();
+
+        // if language form was hidden, show it again
+        $('#lang_form').show();
+
+        $(window).unbind('resize', this.resizeHandler);
+
+        this.opened = false;
+        Overlay.openCount--;
+        if (Overlay.openCount == 0) {
+            Slideshow.resume();
+        }
+    };
+
+    Overlay.prototype.toggle = function()
+    {
+        if (this.opened) {
+            this.close();
+        } else {
+            this.open();
+        }
+    };
+
+    var Slideshow = function(selector)
+    {
+        var that = this;
+        this.selector = selector;
+        this.index = 0;
+        this.opened = false;
+        $(document).ready(function() {
+            that.init();
+        });
+        Slideshow.slideshows.push(this);
+    };
+
+    Slideshow.slideshows = [];
+
+    Slideshow.pause = function()
+    {
+        for (var i = 0; i < Slideshow.slideshows.length; i++) {
+            Slideshow.slideshows[i].stopInterval();
+        }
+    };
+
+    Slideshow.resume = function()
+    {
+        for (var i = 0; i < Slideshow.slideshows.length; i++) {
+            Slideshow.slideshows[i].startInterval();
+        }
+    };
+
+    Slideshow.prototype.init = function()
+    {
+        this.$el = $(this.selector);
+        this.height = this.$el.height();
+        this.$pages = this.$el.children();
+        this.$pages.hide();
+        this.$currentPage = $(this.$pages.get(this.index));
+        this.$currentPage.show();
+        this.startInterval();
+    };
+
+    Slideshow.prototype.nextPage = function()
+    {
+        var that = this;
+        this.$currentPage.animate(
+            { left: 70, opacity: 0 },
+            200,
+            'linear',
+            function() {
+                that.$currentPage.css('display', 'none');
+                that.index++;
+                if (that.index < that.$pages.length) {
+                    that.$currentPage = $(that.$pages.get(that.index));
+                    that.$currentPage
+                        .css('opacity', 0)
+                        .css('display', 'block')
+                        .css('left', 30);
+
+                    var height = that.$currentPage.height();
+                    that.$currentPage.css('top', (that.height - height) / 2);
+
+                    that.$currentPage.animate(
+                        { left: 50, opacity: 1 },
+                        200,
+                        'linear'
+                    );
+                } else {
+                    that.close();
+                }
+            }
+        );
+    };
+
+    Slideshow.prototype.open = function()
+    {
+        this.$el.show();
+        var height = this.$currentPage.height();
+        this.$currentPage.css('top', (this.height - height) / 2);
+        this.opened = true;
+    };
+
+    Slideshow.prototype.close = function()
+    {
+        if (this.opened) {
+            var that = this;
+            this.stopInterval();
+            this.$el.fadeOut(100, function() {
+                that.$el.trigger('close');
+            });
+            this.opened = false;
+        }
+    };
+
+    Slideshow.prototype.startInterval = function()
+    {
+        if (this.index < this.$pages.length) {
+            var that = this;
+            this.interval = setInterval(
+                function() {
+                    that.nextPage();
+                }, 4000
+            );
+        }
+    };
+
+    Slideshow.prototype.stopInterval = function()
+    {
+        if (this.interval !== null) {
+            clearInterval(this.interval);
+            this.intereval = null;
+        }
+    };
+
+    var returning = new Overlay('#overlay-returning');
+    var nonfx     = new Overlay('#overlay-nonfx');
+    var faq       = new Overlay('#overlay-faq');
+
+    var slideshow = new Slideshow('#intro');
+
+    $(document).ready(function() {
+        $('#overlay-open-faq').click(function(e) {
+            e.preventDefault();
+            faq.open();
+        });
+
+        $('#cute').click(function(e) {
+            e.preventDefault();
+            nonfx.close();
+        });
+
+        var $video = $('#video');
+
+        var historyCookieName = 'FirefoxLiveHistory';
+        var history = getCookie(historyCookieName);
+        setCookie(historyCookieName, 'visited', '/', 31 * 24 * 60 * 60);
+
+        if (history == 'visited') {
+            returning.open();
+        } else {
+            $video.hide();
+            slideshow.open();
+            slideshow.$el.bind('close', function() {
+                $video.show();
+            });
+            if (!isFirefox) {
+                var $detected    = $('#detected');
+                var $notdetected = $('#notdetected');
+
+                var isSafari = /Safari/.test(window.navigator.userAgent);
+                var isChrome = /Chrome/.test(window.navigator.userAgent);
+                var isIE     = /MSIE/.test(window.navigator.userAgent);
+
+                if (isChrome) {
+                    $detected.text(
+                        $detected.text().replace(/%BROWSER%/, 'Google Chrome')
+                    );
+                    $notdetected.hide();
+                } else if (isSafari) {
+                    $detected.text(
+                        $detected.text().replace(/%BROWSER%/, 'Safari')
+                    );
+                    $notdetected.hide();
+                } else if (isIE) {
+                    $detected.text(
+                        $detected.text().replace(/%BROWSER%/, 'Internet Explorer')
+                    );
+                    $notdetected.hide();
+                } else {
+                    $detected.hide();
+                }
+
+                nonfx.open();
+            }
+        }
+    });
+
+
+    // ]]>
+    </script>
 
 </body>
 </html>
