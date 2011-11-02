40af4f2265ef41fd1ffe30f14799e56980d8ccf7
steven@silverorange.com <steven@silverorange.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-11-02 19:06:44 +0000

    Bug 697283 - Add Webtrends Tracking for Firefox Live 2
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@97123 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/l10n/firefoxlive.inc.php
+++ includes/l10n/firefoxlive.inc.php
@@ -553,5 +553,11 @@ require "{$config['file_root']}/includes/js_stats.inc.php";
     // ]]>
     </script>
 
+<?
+// Webtrends stats, see bug 556384
+require "{$config['file_root']}/includes/js_stats.inc.php";
+echo $stats_js;
+?>
+
 </body>
 </html>
