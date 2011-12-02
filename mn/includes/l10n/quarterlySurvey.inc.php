ce9b2eea1d5589d73d2f1ab6eebd51def1e37031
channy@mozilla.or.kr <channy@mozilla.or.kr@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-12-02 15:39:36 +0000

    Bug 695645 - [mn] Firefox Web parts for Mongolian
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@98347 4eb1ac78-321c-0410-a911-ec516a8615a5

--- mn/includes/l10n/quarterlySurvey.inc.php
+++ mn/includes/l10n/quarterlySurvey.inc.php
@@ -1,4 +1,4 @@
         <div class="sub-feature hide" id="survey">
-                <h3>Help Us Make Firefox Even Better!</h3>
-                <p>Take a few moments to take a quick survey and share your thoughts with us. <a href="<?=$localesForSurvey[$lang];?>" class="blocklevel">Firefox User Survey</a></p>
+                <h3>Firefox-г илүү сайн болгоход бидэнд туслана уу!</h3>
+                <p>Хэдхэн хормоо бидэнтэй хуваалцаж санал бодлоо хэлнэ үү. <a href="<?=$localesForSurvey[$lang];?>" class="blocklevel">Firefox-н хэрэглэгчийн судлагаа</a></p>
         </div>
