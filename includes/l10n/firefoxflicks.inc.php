f6b1a6d640c430d495cb51d50dfe50fed850a29f
pascal.chevrel@mozilla-europe.org <pascal.chevrel@mozilla-europe.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-12-05 15:06:40 +0000

    l10n: Firefox Flicks, update templates to factorize code, first test version in fr folder
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@98423 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/l10n/firefoxflicks.inc.php
+++ includes/l10n/firefoxflicks.inc.php
@@ -1,21 +1,32 @@
 <?php
 
-$body_id    = 'firefoxflicks';
 
-// commodity functions for localized pages
-require_once $config['file_root'].'/includes/l10n/toolbox.inc.php';
+
+switch($pageid) {
+    default:
+    case 'firefoxflicks':
+        $folder = '';
+        $body_id = 'firefoxflicks';
+        break;
+    case 'firefoxflicks-brief':
+        $folder = 'brief/';
+        $body_id = 'firefoxflicks-brief';
+        break;
+    case 'firefoxflicks-faq':
+        $folder = 'faq/';
+        $body_id = 'firefoxflicks-faq';
+        break;
+}
+
 
 // temporary include to parallelize webdev and l10n- webdev work on the project
 require_once $config['file_root'].'/includes/l10n/firefoxflicks-helper.inc.php';
 
 require_once $config['file_root'].'/includes/l10n/header-firefoxflicks.inc.php';
 
-?>
+echo '    <section id="primary"><div class="container">';
+require_once $config['file_root'].'/'.$lang.'/firefoxflicks/'.$folder.'content.inc.html';
+echo '    </div></section>';
 
-    <section id="primary"><div class="container">
-    <? require_once $config['file_root'].'/'.$lang.'/firefoxflicks/content.inc.html'; ?>
-    </div></section>
-
-<?
 require_once $config['file_root'].'/includes/l10n/footer-firefoxflicks.inc.php';
-?>
+
