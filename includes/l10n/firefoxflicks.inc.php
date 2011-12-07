45fe98dadc252068e9ca38fa0e3fe6b6bb127838
pascal.chevrel@mozilla-europe.org <pascal.chevrel@mozilla-europe.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-12-07 16:26:23 +0000

    Firefox flicks, move links to variables, remove mentions of the Legal and Judges pages
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@98480 4eb1ac78-321c-0410-a911-ec516a8615a5

--- includes/l10n/firefoxflicks.inc.php
+++ includes/l10n/firefoxflicks.inc.php
@@ -1,36 +1,48 @@
 <?php
 
-
-
 switch($pageid) {
     default:
     case 'firefoxflicks':
-        $folder     = '';
-        $body_id    = 'firefoxflicks';
-        $return_home  = '';
+        $folder      = '';
+        $body_id     = 'firefoxflicks';
+        $return_home = '';
         break;
     case 'firefoxflicks-brief':
-        $folder     = 'brief/';
-        $body_id    = 'firefoxflicks-brief';
+        $folder      = 'brief/';
+        $body_id     = 'firefoxflicks-brief';
         $return_home = '<a href="../" class="home">' . ___('« Return Home') . '</a>';
+        $link = array(
+            0 => 'http://www.mozilla.org/en-US/firefox/brand/platform/',
+            1 => 'http://www.mozilla.org/en-US/firefox/brand/',
+            2 => 'mailto:firefoxflicks@mozilla.org',
+            3 => 'http://popcornjs.org/',
+        );
         break;
     case 'firefoxflicks-faq':
-        $folder     = 'faq/';
-        $body_id    = 'firefoxflicks-faq';
+        $folder      = 'faq/';
+        $body_id     = 'firefoxflicks-faq';
         $return_home = '<a href="../" class="home">' . ___('« Return Home') . '</a>';
+        $link = array(
+            0 => '../brief/',
+            1 => 'mailto:firefoxflicks@mozilla.org',
+            2 => 'http://mozilla.org/en-US/firefox/brand/',
+            3 => 'http://www.mozilla.org/foundation/trademarks/policy.html',
+            4 => 'http://search.creativecommons.org/',
+            5 => 'http://en.wikipedia.org/wiki/Public_domain',
+        );
         break;
 }
 
 
-// temporary include to parallelize webdev and l10n- webdev work on the project
-require_once $config['file_root'].'/includes/l10n/firefoxflicks-helper.inc.php';
+// temporary include to parallelize webdev and l10n-webdev work on the project
+require_once $config['file_root'] . '/includes/l10n/firefoxflicks-helper.inc.php';
 
-require_once $config['file_root'].'/includes/l10n/header-firefoxflicks.inc.php';
+require_once $config['file_root'] . '/includes/l10n/header-firefoxflicks.inc.php';
 
 echo '    <section id="primary"><div class="container">';
 echo $return_home;
-require_once $config['file_root'].'/'.$lang.'/firefoxflicks/'.$folder.'content.inc.html';
+require_once $config['file_root'] . '/' . $lang . '/firefoxflicks/' . $folder . 'content.inc.html';
 echo '    </div></section>';
 
-require_once $config['file_root'].'/includes/l10n/footer-firefoxflicks.inc.php';
+require_once $config['file_root'] . '/includes/l10n/footer-firefoxflicks.inc.php';
 
