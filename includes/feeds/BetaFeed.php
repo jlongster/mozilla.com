0cd4fb49a9f0d27e4471cb5a68995e13bf35e6ce
steven@silverorange.com <steven@silverorange.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-12-19 17:03:34 +0000

    Bug 684089 - [Firefox Beta] Implement: New Firefox Beta Page
    git-svn-id: svn+ssh://svn.mozilla.org/projects/mozilla.com/trunk@99079 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ includes/feeds/BetaFeed.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once dirname(__FILE__) . '/RapidReleaseFeed.php';
+
+/**
+ * @package   MozillaCom
+ * @author    Steven Garrrity <steven@silverorange.com>
+ * @author    Michael Gauthier <mike@silverorange.com>
+ * @copyright 2011 Mozilla Foundation, 2011 silverorange
+ * @license   http://www.mozilla.org/MPL/MPL-1.1.txt Mozilla Public License
+ */
+class BetaFeed extends RapidReleaseFeed
+{
+    protected function getURI()
+    {
+        return 'http://blog.mozilla.com/futurereleases/category/firefox-beta/feed/';
+    }
+}
+
+?>
